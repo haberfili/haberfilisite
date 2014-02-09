5488805abecabc1358edc223a4c1c2339594583a
mokarakaya <mozgkarakaya@gmail.com>
2014-02-09 17:44:17 +0200

    add tr language support

---
+++ app/controllers/Language.java
@@ -0,0 +1,72 @@
+package controllers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import models.News;
+import models.User;
+import mongo.DBConnector;
+import play.Routes;
+import play.data.Form;
+import play.i18n.Lang;
+import play.mvc.*;
+import play.mvc.Http.Response;
+import play.mvc.Http.Session;
+import providers.MyUsernamePasswordAuthProvider;
+import providers.MyUsernamePasswordAuthProvider.MyLogin;
+import providers.MyUsernamePasswordAuthProvider.MySignup;
+
+import views.html.*;
+import be.objectify.deadbolt.java.actions.Group;
+import be.objectify.deadbolt.java.actions.Restrict;
+
+import com.feth.play.module.pa.PlayAuthenticate;
+import com.feth.play.module.pa.providers.password.UsernamePasswordAuthProvider;
+import com.feth.play.module.pa.user.AuthUser;
+import com.google.code.morphia.Datastore;
+
+public class Language extends Controller {
+
+	public static final String FLASH_MESSAGE_KEY = "message";
+	public static final String FLASH_ERROR_KEY = "error";
+	public static final String USER_ROLE = "user";
+	
+	public static Result de() throws Exception{
+		
+		changeLang("de");
+		List<News> newsList = null;
+		try {
+			DBConnector connector= new DBConnector();
+			Datastore datasource = connector.getDatasource();
+			newsList = datasource.find(News.class).asList();
+		} catch (Exception e) {
+			throw e;
+		}
+		return ok(index.render(newsList));
+	}
+	public static Result en() throws Exception{
+		changeLang("en");
+		List<News> newsList = null;
+		try {
+			DBConnector connector= new DBConnector();
+			Datastore datasource = connector.getDatasource();
+			newsList = datasource.find(News.class).asList();
+		} catch (Exception e) {
+			throw e;
+		}
+		return ok(index.render(newsList));
+	}
+	public static Result tr() throws Exception{
+		changeLang("tr");
+		List<News> newsList = null;
+		try {
+			DBConnector connector= new DBConnector();
+			Datastore datasource = connector.getDatasource();
+			newsList = datasource.find(News.class).asList();
+		} catch (Exception e) {
+			throw e;
+		}
+		return ok(index.render(newsList));
+	}
+}
