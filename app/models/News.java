383531a2ce1a2fc3eb11008a645aef85d893e0af
mokarakaya <mozgkarakaya@gmail.com>
2014-07-13 01:19:46 +0300

    add similar news

--- app/models/News.java
+++ app/models/News.java
@@ -1,14 +1,20 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.types.ObjectId;
 
 import com.google.code.morphia.annotations.Entity;
 import com.google.code.morphia.annotations.Id;
+import com.google.code.morphia.annotations.Reference;
 
 @Entity("news")
 public class News  {
 	 @Id public ObjectId id;
 	 public String title,detail,link,image,source,detailMore,category;
+	 @Reference(lazy = true)  
+	 public List<News> similarNews = new ArrayList<News>();
 	 public long createDate;
  
 
